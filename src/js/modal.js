e5cf5f76f706214389e0c712329dad02f30fdc3a
Lucas Ferreira <155773773+LuksDevs@users.noreply.github.com>
2025-04-04 01:34:48 -0300

    Área de tarefas
    Layout e script de Registro de tarefas concluído.

---
+++ src/js/modal.js
@@ -0,0 +1,44 @@
+
+
+
+
+export function showModal(rgb, icon, text) {
+
+    let displayModal = document.getElementById('showmodal')
+    if(displayModal) {
+      displayModal.remove()
+    }
+
+    let modal = document.createElement('div')
+        modal.innerHTML = `${icon} ${text}`
+        modal.style.backgroundColor = rgb
+        modal.style.color = '#fff'
+        modal.style.padding = '10px'
+        modal.style.fontSize = '16px'
+        modal.style.borderRadius = '5px'
+        modal.style.position = 'absolute'
+        modal.style.top = '-30%'
+        modal.style.opacity = '0'
+        modal.style.left = '50%'
+        modal.style.transform = 'translateX(-50%)'
+        modal.style.transition = 'top .6s ease-in-out, opacity .6s ease-in-out'
+        modal.style.zIndex = '1000'
+        modal.id = 'showmodal'
+        
+    document.body.appendChild(modal)
+
+    setTimeout(() => {
+        modal.style.top = '10%'
+        modal.style.opacity = '1'
+    }, 10)
+
+    setTimeout(()=> {
+        modal.style.top = '-30%'
+        modal.style.opacity = '0'
+        setTimeout(()=> {
+            if(document.body.contains(modal)){
+                document.body.removeChild(modal)
+            }
+        }, 1000)
+    }, 2000)
+}
